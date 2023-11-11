bca934b5eccd724691428dc814562e6edb925e3c
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-11-11 11:14:08 +0100

    add hero section with background image and titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.scss";
+import AboutSection from "./components/about/AboutSection";
+import HeroSection from "./components/hero/HeroSection";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
+      <AboutSection />
     </div>
   );
 }
