1f7ae205e04943b7002f69e9978fe0ec9311dab8
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-11-12 17:27:01 +0100

    change background mage

--- src/components/hero/HeroSection.tsx
+++ src/components/hero/HeroSection.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <section className="heroSection" id="home">
       <h2>Marie-Hermine Schneider</h2>
-      <h3>Développeuse web</h3>
+      <h3>Développeuse JS React</h3>
     </section>
   );
 }
