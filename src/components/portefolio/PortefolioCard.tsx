eb27bd87195137efc4b87a773865e4f96649cca2
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-11-14 08:45:12 +0100

    fix ts error

--- src/components/portefolio/PortefolioCard.tsx
+++ src/components/portefolio/PortefolioCard.tsx
@@ -25,15 +25,17 @@ export default function PortefolioCard({ project }: Props) {
               <h5>{project.title}</h5>
               <p>{project.description}</p>
               <ul className="portefolioSection__gridCardList">
-                {project.techno.map((tech: IconDefinition, index: number) => {
-                  const image =
-                    typeof tech === "string" ? (
-                      <img src={tech} alt="techno" />
-                    ) : (
-                      <FontAwesomeIcon icon={tech} />
-                    );
-                  return <li key={index}>{image}</li>;
-                })}
+                {project.techno.map(
+                  (tech: string | IconDefinition, index: number) => {
+                    const image =
+                      typeof tech === "string" ? (
+                        <img src={tech} alt="techno" />
+                      ) : (
+                        <FontAwesomeIcon icon={tech} />
+                      );
+                    return <li key={index}>{image}</li>;
+                  }
+                )}
               </ul>
             </div>
           </div>
